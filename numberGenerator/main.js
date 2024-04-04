38652e86960caf553b892e58eb69360318a48a79
Adesh Shah <shah0150@algonquinlive.com>
2024-04-04 09:44:06 -0400

    added questions

---
+++ numberGenerator/main.js
@@ -0,0 +1,1 @@
+// Create a div container on HTML document and create 1 to 100 numbers dynamically and append to the container div. Even numbers background is green. Odd numbers background is yellow. Prime numbers background is red.
---
+++ weightCalculator/main.js
@@ -0,0 +1,2 @@
+// Develop a small application in javascript which calculate a weight of an object in a certain planet. 
+
